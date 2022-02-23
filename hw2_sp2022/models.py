658dcac109a399628a359607cb9fe09f87471576
Cassie Parent <clparent121@gmail.com>
2022-02-22 20:24:21 -0500

    rough draft of sgd

--- hw2_sp2022/models.py
+++ hw2_sp2022/models.py
@@ -36,15 +36,32 @@ class LogisticRegressionSGD(Model):
     def __init__(self, n_features, learning_rate = 0.1):
         super().__init__()
         # TODO: Initialize parameters, learning rate
-        pass
+        self.w = np.zeros((n_features, 1))
+        self.lr = learning_rate
+        
 
     def fit(self, X, y):
-        # TODO: Write code to fit the model
-        pass
+        w = self.w
+        lr = self.lr
+        X = X.toarray()
+        ft_num = w.shape[0]
+        print(X.shape)
+        for i in range(X.shape[0]):
+            for j in range(w.shape[0]):
+                sig = np.matmul(w.transpose(), X[i][:].reshape((ft_num, 1)))
+                w[j] = w[j] + (lr * X[i][j]) * (y[i] - sigmoid(sig[0]))
+        self.w = w
 
     def predict(self, X):
         # TODO: Write code to make predictions
-        pass
+        X = X.toarray()
+        res = sigmoid(np.matmul(X, self.w))
+        for i in range(res.shape[0]):
+            if res[i][0] < .5:
+                res[i][0] = 0
+            else:
+                res[i][0] = 1
+        return (res)
 
 class LogisticRegressionNewton(Model):
 
