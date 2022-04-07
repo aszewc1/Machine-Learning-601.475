82acf7f7e89e0aeedb4ce29bbe40509737bc6f89
Cassie Parent <clparent121@gmail.com>
2022-04-07 14:38:59 -0400

    finalized hac with comments, poor draft of lambda

--- hw4_sp2022/model_HAC.py
+++ hw4_sp2022/model_HAC.py
@@ -5,39 +5,53 @@ This file is where you will write all of your code!
 """
 
 import numpy as np
+
+#calculating single method between clusters
 def single(clust1, clust2, X):
+    #start with arbitrarily high distance
     best_dist = 1000000000
+    #get the indexes of x for each cluster
     cluster1 = clust1.tolist()
     cluster2 = clust2.tolist()
     for k in range(len(cluster1)):
         for l in range(len(cluster2)):   
+            #calculate distance and update if it's better
             curr_dist = np.linalg.norm(X[cluster1[k]][:] - X[cluster2[l]][:])
             if curr_dist < best_dist:
                 best_dist = curr_dist
     return best_dist
 
+#calculating complete method between clusters
 def complete(clust1, clust2, X):
+    #set distance arbitrarily low
     best_dist = 0
     cluster1 = clust1.tolist()
     cluster2 = clust2.tolist()
     for k in range(len(cluster1)):
         for l in range(len(cluster2)):   
+            #calculate distance and if larger update
             curr_dist = np.linalg.norm(X[cluster1[k]][:] - X[cluster2[l]][:])
             if curr_dist > best_dist:
                 best_dist = curr_dist
+    #if clusters are empty, return arbitrarily high distance so it won't be kept
     if(len(cluster1) == 0 or len(cluster2)==0):
         best_dist = 10000000
     return best_dist
 
+#calculate average method between clusters
 def average(clust1, clust2, X):
+    #initialize sum to zero
     sum = 0
     cluster1 = clust1.tolist()
     cluster2 = clust2.tolist()
     for k in range(len(cluster1)):
         for l in range(len(cluster2)):
+            #add to total distance
             sum += np.linalg.norm(X[cluster1[k]][:] - X[cluster2[l]][:])
+    #if either cluster is empty, return arbitrarily high distance
     if(len(cluster1) == 0 or len(cluster2)==0):
         return 10000000
+    #compute average and return
     sum /= (len(cluster1)*len(cluster2))
     return sum
 
@@ -84,19 +98,21 @@ class AgglomerativeClustering(Model):
         Returns:
             A dense array of ints with shape [num_examples].
         """
-        # TODO: Implement this!
-
+        #initialize labels for clusters from 0 to shape of X
         labels = list(range(0, X.shape[0]))
         
         num_clusters = self.n_clusters
         curr_clust_num = (np.unique(np.array(labels))).shape[0]
+        #while more clusters exist than desired final number
         while(curr_clust_num > num_clusters):
-            print((np.unique(np.array(labels))).shape[0])
+            #set best parameters arbitrarily high
             best_dist = 10000000
             best_i_ind = 0
             best_j_ind = 10
+            #cycle through all possible clusters
             for i in range(X.shape[0]):
                 for j in range(X.shape[0]):
+                    #if two belong to the same cluster, skip
                     if i == j or labels[i] == labels[j]:
                         continue
 
@@ -104,6 +120,7 @@ class AgglomerativeClustering(Model):
                     clust1 = np.where(np.array(labels) == i)[0]
                     clust2 = np.where(np.array(labels) == j)[0]
 
+                    #check method
                     if self.linking == "single":
                         curr_dist = single(clust1, clust2, X)
                     elif self.linking == "complete":
@@ -129,12 +146,14 @@ class AgglomerativeClustering(Model):
                             best_i_ind = i_ind
                             best_j_ind = j_ind
 
+            #merge two clusters with the smallest distance
             new_cluster = labels[best_i_ind]
             old_cluster = labels[best_j_ind]
             labels = [new_cluster if item == old_cluster else item for item in labels]
             
             curr_clust_num = (np.unique(np.array(labels))).shape[0]
-
+            
+        #get rid of large cluster numbers
         for i in range(num_clusters):
             if i in labels:
                 continue
--- hw4_sp2022/model_lambda_means.py
+++ hw4_sp2022/model_lambda_means.py
@@ -64,7 +64,9 @@ class LambdaMeans(Model):
             nfeatures: size of feature space (only needed for _fix_test_feats)
             lambda0: A float giving the default value for lambda
         """
-        # TODO: Initializations etc. go here.
+        #initialize parameters
+        self.nfeatures = nfeatures 
+        self.lambda0 = lambda0
 
     def fit(self, *, X, iterations):
         """
@@ -77,7 +79,27 @@ class LambdaMeans(Model):
             iterations: int giving number of clustering iterations
         """
         # TODO: Implement this!
-        raise Exception("You must implement this method!")
+        
+        X = X.toarray()
+        print(X)
+        #cluster initialization
+        mu = [sum(X)/len(X)]
+        clusters = 1
+        #repeat for given number of iterations
+        for i in range(iterations):
+            #e step
+            for k in range(clusters):
+                r = 0
+                if k == np.linalg.norm(X[i] - mu[i]) and np.linalg.norm(X[i] - mu) <= self.lambda0:
+                    r = 1
+                r_new_clust = 0
+                if np.linalg.norm(X[i] - mu[i]) > self.lambda0:
+                    r_new_clust = 1
+                if r_new_clust == 1:
+                    mu[i+1] = X[i]
+            #mstep
+            for k in range(clusters):
+                mu[k] = sum(r*X[i])/sum(r)
 
     def predict(self, X):
         """ Predict.
